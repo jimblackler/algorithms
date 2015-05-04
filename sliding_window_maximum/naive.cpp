27f6e7ab058f229737cfad854a5ed1d69b1c7396
Jim Blackler <jimblackler@gmail.com>
2015-05-05 00:35:06 +0100

    Adding missing file.

---
+++ sliding_window_maximum/naive.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include "naive.h"
+
+namespace slidingWindowMaximum {
+
+void naive(const int *in, size_t length, size_t windowSize, int *out) {
+
+  for (int i = 0; i < length; i++) {
+    int v = 0;
+    for (int j = i; j >= 0 && j > i - windowSize; j--) {
+      v = std::max(v, in[j]);
+    }
+    out[i] = v;
+  }
+}
+
+}
